fa5297f5df2e7432de9a2dafcbbe9ed3da494699
Matteo G <matteo.gennari2411@gmail.com>
2023-07-10 23:43:29 +0200

    ordinamento storico

--- GESTIONE-SPESE-SPRING/gestorespese/src/main/java/it/progetticasa/gestorespese/services/investimenti/StoricoUtenteInvestimentoService.java
+++ GESTIONE-SPESE-SPRING/gestorespese/src/main/java/it/progetticasa/gestorespese/services/investimenti/StoricoUtenteInvestimentoService.java
@@ -1,6 +1,7 @@
 package it.progetticasa.gestorespese.services.investimenti;
 
 import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.sql.rowset.serial.SerialException;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import it.progetticasa.gestorespese.model.investimenti.StoricoUtenteInvestimento;
+import it.progetticasa.gestorespese.model.spese.StoricoSpese;
 import it.progetticasa.gestorespese.repositories.investimenti.StoricoUtenteInvestimentoRepository;
 
 @Service
@@ -32,6 +34,17 @@ public class StoricoUtenteInvestimentoService {
 		return (List<StoricoUtenteInvestimento>) quoteInvestimentoRepository.findAll();
 	}
 	public List<StoricoUtenteInvestimento> ricerca(StoricoUtenteInvestimento asProbe) throws SerialException, SQLException {
-        return quoteInvestimentoRepository.findAll(Example.of(asProbe));
+		List<StoricoUtenteInvestimento> storico = quoteInvestimentoRepository.findAll(Example.of(asProbe));
+        storico.sort(new Comparator<StoricoUtenteInvestimento>() {
+            @Override
+            public int compare(StoricoUtenteInvestimento m1, StoricoUtenteInvestimento m2) {
+                if(m1.getDataOperazione().after(m2.getDataOperazione())){
+                    return -1;
+                }else {
+                	return 1;
+                }
+             }
+        });
+		return storico;
     }
 }
